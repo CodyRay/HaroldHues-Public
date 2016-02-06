f04f1791a090fc02dd0b44587c1f74fc7c66965f
Cody Ray Hoeft <crhoeft@gmail.com>
2016-02-05 17:01:01 -0800

    It is funny the things that we hold on to

--- scripts/Create many folders.bash
+++
@@ -1,4 +0,0 @@
-NUMFOLDERS=31
-
-for ((x=1;x< = $NUMFOLDERS;x+=1)); do mkdir `printf "%02d" $x`; done
-
--- scripts/HSD/Rhino Silent Install/CopyLocal.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-robocopy "\\hsd.local\software\Repo\Deployment\Rhinoceros 5" "C:/users/public/documents/TMPRhino" /mir
-pause
