71e84afc16d166d15fe16801ff66edae77904e38
Cody Ray Hoeft <crhoeft@gmail.com>
2016-02-05 17:06:12 -0800

    Ugh, CentOs is so much nicer than Ubuntu

--- scripts/Update Git Bash.bash
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-DATE =`date -I`
-
-git add Code &&
-git commit -m "Automatic Commit: $DATE" &&
-git push
--- scripts/removespaces.bash
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-ls | grep '[a-zA-Z0-9]\ [a-zA-Z0-9]' | while read line 
-do
-mv "$line" $( echo $line | sed s/' '/'_'/g )
-done
